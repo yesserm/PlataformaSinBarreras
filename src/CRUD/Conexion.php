5571bd7673c60e2c8b9bbce5282591a00f8241db
yessermiranda13 <yesser.miranda13@est.unanleon.edu.ni>
2020-08-11 15:25:15 -0600

    se realiza la conexion a la base de datos

---
+++ src/CRUD/Conexion.php
@@ -0,0 +1,12 @@
+<?php
+    include '..\\Utils\\Config.php';
+
+    $config = Config::getInstancia()->get('db');
+    $db = new PDO(
+        'mysql:host=127.0.0.1;dbname=libreria',
+        $config["db"]["user"],
+        $config["db"]["password"]
+    );
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+
+?>
