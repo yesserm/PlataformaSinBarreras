b79b6b7bde843f9e139d6d9bf46807568ed7cccd
Henry <henrydiaz1202@gmail.com>
2020-08-11 11:49:00 -0600

    movi el perfil instit a la carpeta de barreras lo habia creado afuera xd

---
+++ perfil_instit.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Area - perfil institución</title>
+    <link rel="stylesheet" href="css/estilo_perfil_inst.css">
+</head>
+<body>
+    <form action="POST">
+
+    </form>
+</body>
+</html>
