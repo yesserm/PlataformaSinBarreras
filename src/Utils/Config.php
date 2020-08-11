0ebcf73fd89bbf1a1d1c8db43d020e499047e085
yessermiranda13 <yesser.miranda13@est.unanleon.edu.ni>
2020-08-11 14:57:54 -0600

    Utilizacion del patron singleton para las instancias de la bd

---
+++ src/Utils/Config.php
@@ -0,0 +1,27 @@
+<?php
+    include 'NotFoundException.php';
+
+    class Config{
+        private $datos;
+        private static $instancia;
+
+        private function __construct(){
+            $json = file_get_contents(dirname(__DIR__,2) . '\config\app.json');
+            $this->datos = json_decode($json,true);
+        }
+
+        public static function getInstancia(){
+            if(self::$instancia == null){
+                self::$instancia = new Config();
+            }
+            return self::$instancia;
+        }
+        public function get($key){
+            if(!isset($this->datos[$key])){
+                throw new NotFoundException("La clave $key no esta en la configuracion");
+            }
+            return $this->datos;
+        }
+    }
+
+?>
